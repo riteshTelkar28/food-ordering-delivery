84003ab98ee68c7692d0466f3d380ee2d1c9ef11
riteshTelkar28 <riteshtelkar28@gmail.com>
2025-01-10 15:46:38 +0530

    changes made

--- model/orderSchema.js
+++ model/orderSchema.js
@@ -1,12 +1,7 @@
 import mongoose from "mongoose";
 import url from "../database/connection.js";
 
-mongoose.connect(url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    serverSelectionTimeoutMS:5000,
-    tls:true
-});
+mongoose.connect(url);
 
 const orderSchema = mongoose.Schema({
     email:{
